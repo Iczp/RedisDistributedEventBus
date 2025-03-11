ee943f6e6f54b57fb262bbd88597089a3b57ca4a
zhongpei <1000@intry.cn>
2025-03-11 16:07:36 +0800

    DistributedEventSentHandler/DistributedEventReceivedHandler

---
+++ src/IczpNet.RedisDistributedEventBus.Domain/Handlers/DistributedEventReceivedHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.EventBus;
+using Volo.Abp.Domain.Services;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Volo.Abp.Json;
+
+namespace IczpNet.RedisDistributedEventBus.Handlers;
+
+public class DistributedEventReceivedHandler(IJsonSerializer jsonSerializer) : DomainService, ILocalEventHandler<DistributedEventReceived>, ITransientDependency
+{
+    public IJsonSerializer JsonSerializer { get; } = jsonSerializer;
+
+    public async Task HandleEventAsync(DistributedEventReceived eventData)
+    {
+        // TODO: IMPLEMENT YOUR LOGIC...
+
+        Logger.LogInformation($"收到分布式事件[{eventData.Source}]：EventName={eventData.EventName},EventData={JsonSerializer.Serialize(eventData.EventData)}");
+
+        await Task.CompletedTask;
+    }
+}
---
+++ src/IczpNet.RedisDistributedEventBus.Domain/Handlers/DistributedEventSentHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.EventBus;
+using Volo.Abp.Domain.Services;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.Json;
+
+namespace IczpNet.RedisDistributedEventBus.Handlers;
+
+public class DistributedEventSentHandler(IJsonSerializer jsonSerializer) : DomainService, ILocalEventHandler<DistributedEventSent>, ITransientDependency
+{
+    public IJsonSerializer JsonSerializer { get; } = jsonSerializer;
+
+    public async Task HandleEventAsync(DistributedEventSent eventData)
+    {
+        Logger.LogInformation($"发送分布式事件[{eventData.Source}]：EventName=${eventData.EventName},EventData={JsonSerializer.Serialize(eventData.EventData)}");
+
+        await Task.CompletedTask;
+    }
+}
